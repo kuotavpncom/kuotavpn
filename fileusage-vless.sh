4710b9ecb90b4e9cb6e33ec3f2aa8733c9107fce
arifrizqi <arifrizqy.ar15@gmail.com>
2025-02-05 18:01:35 +0700

    init

--- fileusage-vless.sh
+++ fileusage-vless.sh
@@ -79,7 +79,7 @@ repogithub='kuotavpncom/kuotavpn/main'
 repopermission='https://raw.githubusercontent.com/kuotavpncom/ip/main/permission.txt'
 curl -s -f -H 'Cache-Control: no-cache, no-store' $repopermission -o /tmp/permission.txt
 if [ $? -ne 0 ]; then
-  repopermission='https://www.kuotavpn.com/ZGFmdGFySVA/${IP)'
+  repopermission='https://www.kuotavpn.com/ZGFmdGFySVA/${IP}'
   curl -s -f -H 'Cache-Control: no-cache, no-store' $repopermission -o /tmp/permission.txt
   if [ $? -ne 0 ]; then
     repopermission='https://raw.githubusercontent.com/kuotavpncom/ip/main/permission.txt'
